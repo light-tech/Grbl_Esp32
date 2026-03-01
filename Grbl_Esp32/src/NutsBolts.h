fb1a0f06f84eebb4d1aa38677842439b3a2fa1f2
Lightech <lightech@outlook.com>
2026-03-01 09:28:10 +0700

    Fix warning about redefining macro MIN, MAX

--- Grbl_Esp32/src/NutsBolts.h
+++ Grbl_Esp32/src/NutsBolts.h
@@ -66,8 +66,12 @@ const double INCH_PER_MM = (0.0393701);
 #define clear_vector(a) memset(a, 0, sizeof(a))
 #define clear_vector_float(a) memset(a, 0.0, sizeof(float) * MAX_N_AXIS)
 // #define clear_vector_long(a) memset(a, 0.0, sizeof(long)*MAX_N_AXIS)
+#ifndef MAX
 #define MAX(a, b) (((a) > (b)) ? (a) : (b))  // changed to upper case to remove conflicts with other libraries
+#endif
+#ifndef MIN
 #define MIN(a, b) (((a) < (b)) ? (a) : (b))  // changed to upper case to remove conflicts with other libraries
+#endif
 #define isequal_position_vector(a, b) !(memcmp(a, b, sizeof(float) * MAX_N_AXIS))
 
 // Bit field and masking macros
